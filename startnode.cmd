388f1e8d5211c83b32ca202d02992e12b8a765db
nickrawlings2012 <33588906+nickrawlings2012@users.noreply.github.com>
2022-07-18 15:58:01 +0100

    Add files via upload

---
+++ startnode.cmd
@@ -0,0 +1,1 @@
+geth --networkid 4224 --mine --datadir "~/ChainSkills/private" --nodiscover --rpc --rpcport "8545" --port "30303" --rpccorsdomain "*" --nat "any" --rpcapi eth,web3,personal,net --unlock 0 --password password.sec
